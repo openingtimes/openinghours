0ac90fff14e1c6e763a6e8f4064cf0351217f415
Opening Times <114826762+openingtimes@users.noreply.github.com>
2022-10-01 19:05:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,34 @@ export default function Home() {
         <p className="description">
           Betting Shop Store Opening Times Near Me 
         </p>
+    <p>
+    <a href="https://betopeningtimes.co.uk/ladbrokes">Ladbrokes opening times</a>
+    </p>
+    <p>
+    <a href="https://betnearme.co.uk/ladbrokes">Ladbrokes near me</a>
+    </p>
+    <p>
+    <a href="https://closingtimes.co.uk/ladbrokes">Ladbrokes closing times</a>
+    </p>
+    <p>
+    <a href="https://openingtimestoday.co.uk/ladbrokes">Ladbrokes opening times today</a>
+    </p>
+    <p>
+    <a href="https://openingtimessunday.co.uk/ladbrokes">Ladbrokes opening times Sunday</a>
+    </p>
+    <p>
+    <a href="https://openingtimessaturday.co.uk/ladbrokes">Ladbrokes opening times Saturday</a>
+    </p>
+    <p>
+    <a href="https://bookieshours.co.uk/ladbrokes-opening-hours">Ladbrokes opening hours</a>
+    </p>
+    <p>
+    <a href="https://openingtoday.co.uk/ladbrokes">Ladbrokes opening times tomorrow</a>
+    </p>
+    <p>
+    <a href="https://openingtimesday.co.uk/">Ladbrokes opening times near me</a>
+    </p>
       </main>
-
       <Footer />
     </div>
   )
